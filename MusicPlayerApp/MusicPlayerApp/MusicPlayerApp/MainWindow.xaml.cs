279286024f751d6e1fb5e39092bf9f536c4f70ec
hsir571 <79824690+hsir571@users.noreply.github.com>
2024-07-18 01:01:08 +1200

    Start project

---
+++ MusicPlayerApp/MusicPlayerApp/MusicPlayerApp/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicPlayerApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,INotifyPropertyChanged
+    {
+
+        public String testing = "Im testing this out";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e) {
+
+            if (e.ChangedButton == MouseButton.Left) {
+                this.DragMove();
+            }
+        
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        public String Test
+        {
+            get => testing;
+            set
+            {
+                if(value == testing)
+                {
+                    return; 
+                }
+                testing = value;
+                OnPropertyChanged(); 
+            }
+        }
+
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Test = "wow i made it work"; 
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
